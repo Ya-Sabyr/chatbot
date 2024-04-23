04bc47903a0bc9f1c3cd0dcfb3d95641074d698b
Ya-Sabyr <sabyrberikuly07@gmail.com>
2024-04-23 18:06:00 +0500

    changed a bit

--- bot/bot/asgi.py
+++ bot/bot/asgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-from chat.routing import websocket_urlpatterns
+from pegasus_chat_demo.routing import websocket_urlpatterns
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from channels.security.websocket import AllowedHostsOriginValidator
